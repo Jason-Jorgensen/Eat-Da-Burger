a88b75663aceacbe476858354adc339644d7f40b
Jason Jorgensen <jasoncjorgensen@gmail.com>
2021-03-14 21:58:16 -0600

    Added the SQL database, Express Server, ORM, and Models

---
+++ config/orm.js
@@ -0,0 +1,46 @@
+
+const connection = require('./connection.js');
+
+const orm = {
+    selectAll(tableInput, cb) {
+        const queryString = 'SELECT * FROM ??'
+        connection.query(
+            queryString,
+            [tableInput],
+            (err,result) => {
+                if (err) {
+                throw err;
+                }
+                cb(result);
+            }
+        );
+    },
+    insertOne(tableInput, columnName, newValueName,cb) {
+        const queryString = 'INSERT INTO ?? (??) VALUES (??)'
+        connection.query(
+            queryString,
+            [tableInput,columnName, newValueName],
+            (err,result) => {
+                if (err) {
+                    throw err;
+                    }
+                cb(result);
+            }
+        );
+    },
+    updateOne(tableInput, columnName, newValueName, searchColName, oldValueName, cb) {
+        const queryString = 'UPDATE ?? SET ?? = ? WHERE ?? = ?'
+        connection.query(
+            queryString,
+            [tableInput, columnName, newValueName, searchColName, oldValueName],
+            (err, result) => {
+                if (err) {
+                    throw err;
+                    }
+                cb(result)
+            }
+        );
+    },
+};
+
+module.exports = orm;
